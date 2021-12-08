f31f73755c4809c4672896b38f7396aa61fd202c
pphoebelemonn <89839326+pphoebelemonn@users.noreply.github.com>
2021-12-08 23:54:01 +0800

    Add files via upload

---
+++ digital-ethnography/digital-ethnography/js/entries-fade-in.js
@@ -0,0 +1,29 @@
+function fadeOnScroll(){
+  let entries = document.getElementsByClassname("entries");
+}
+//
+//
+// $(document).ready(function() {
+//
+//     /* Every time the window is scrolled ... */
+//     $(window).scroll( function(){
+//
+//         /* Check the location of each desired element */
+//         $('.entries').each( function(i){
+//
+//             var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+//
+//             /* If the object is completely visible in the window, fade it it */
+//             console.log(bottom_of_window);
+//             if( bottom_of_window > bottom_of_object ){
+//
+//                 $(this).animate({'opacity':'1'},1500);
+//
+//             }
+//
+//         });
+//
+//     });
+//
+// });
